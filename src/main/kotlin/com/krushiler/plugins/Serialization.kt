1086bce5f0cf01e7d7f75be6afe29108eded775f
Krushiler <daniil.kn2004@gmail.com>
2023-04-21 09:31:18 +0800

    Do not explicit nulls and encode defaults while Serializing DTOs

--- src/main/kotlin/com/krushiler/plugins/Serialization.kt
+++ src/main/kotlin/com/krushiler/plugins/Serialization.kt
@@ -5,12 +5,16 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(
             Json {
+                explicitNulls = false
+                encodeDefaults = true
                 ignoreUnknownKeys = true
             }
         )
